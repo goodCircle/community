34a23a59a355ea1fabf6b17089307bfc0c5c5584
JoJO <976069783@qq.com>
2021-06-06 01:02:13 +0800

    feat:Spring MVC入门

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -4,8 +4,15 @@ import com.nowcoder.community.config.AlphaConfig;
 import com.nowcoder.community.service.AlphaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.util.Enumeration;
 
 @Controller
 @RequestMapping("/alpha")
@@ -24,5 +31,62 @@ public class AlphaController {
     @RequestMapping("/data")
     public String getData() {
         return alphaService.find();
+    }
+
+    @RequestMapping("http")
+    public void http(HttpServletRequest request, HttpServletResponse response) {
+        //获取请求数据
+        System.out.println(request.getMethod());
+        System.out.println(request.getServletPath());
+        Enumeration<String> enumeration = request.getHeaderNames();
+        while (enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + ":" + value);
+        }
+        System.out.println(request.getParameter("code"));
+
+        // 返回响应数据
+        response.setContentType("text/html;charset=utf-8");
+        try {
+            PrintWriter writer = response.getWriter();
+            writer.write("<h1>response牛</h1>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @RequestMapping(path = "/students", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudents(HttpServletRequest request) {
+        System.out.println(request.getParameter("name"));
+        return "get name successfully";
+    }
+
+    @RequestMapping(path = "/students/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudents(@PathVariable("id") int id) {
+        System.out.println(id);
+        return "get id successfully";
+    }
+
+    //POST请求 对应student.html
+    @RequestMapping(path = "/student", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name, int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return "success";
+    }
+
+    // 响应html数据
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTearcher() {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject( "name", "dhy");
+        mav.addObject("age", 24);
+        mav.setViewName("/demo/view");
+        return mav;
     }
 }
